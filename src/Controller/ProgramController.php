a9b39483a3dbb57b79301687492eb856e5734231
Radu BOBES <radu-bobes_student2023@wilder.school>
2023-06-01 16:12:07 +0200

    routing

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,13 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
+
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
-         ]);
+        ]);
+    }
+    
+    #[Route('/{id}', methods: ['GET'], requirements: ['id'=>'\d+'], name: 'program_show')]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id]);
     }
 }
