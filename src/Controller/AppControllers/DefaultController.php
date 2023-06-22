143ed76870acc6776e9e50c1f2fc969ad576a43d
Radu BOBES <radu-bobes_student2023@wilder.school>
2023-06-22 15:12:13 +0200

    separation admin user app crée

---
+++ src/Controller/AppControllers/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\AppControllers;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+        return $this->render('/app/index.html.twig', [
+            'text' => 'Wild Series, baby!',
+         ]);
+    }
+}
